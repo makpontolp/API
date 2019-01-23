28e469a402d2ec0d3db6cf04a38e863c39555509
Marco Sousa <mak.lp@hotmail.com>
2019-01-23 17:52:28 -0200

    api

---
+++ Filter.py
@@ -0,0 +1,17 @@
+from slackclient import SlackClient
+
+import os
+
+slack_token = os.environ["SLACK_API_TOKEN"] = "YOUR_TOKEN"
+sc = SlackClient(slack_token)
+
+grupos_slack = list()
+
+grupos_slack = sc.api_call("channels.list", exclude_archived=1)
+grupos_slack_chanels = (grupos_slack['channels'])
+for item in grupos_slack_chanels:
+    if item['num_members'] < 2:
+
+        print(sc.api_call("channels.archive",channel=item['id']))
+
+   # print('ID: {}, PESSOAS: {}, NOME: {}'.format(item['id'],item['num_members'],item['name']))
--- webAPI.py
+++ webAPI.py
@@ -1,13 +1,18 @@
-import os
 from slackclient import SlackClient
 
-slack_token = os.environ["ZZZZZZ"]
+import os
+
+slack_token = os.environ["SLACK_API_TOKEN"] = "YOUR TOKEN"
 sc = SlackClient(slack_token)
 
-sc.api_call(
-  "channels.info",
-  channel="ZZZZZZZZZZZz",
-  name="thumbsup",
-  timestamp="1234567890.123456",
+grupos_slack = list()
 
-)
+grupos_slack = sc.api_call("channels.list", )
+grupos_slack_chanels = (grupos_slack['channels'])
+for item in grupos_slack_chanels:
+    if item['num_members'] < 2:
+        print('ID: {}, PESSOAS: {}, ARQUIVADO: {}, NOME: {},'.format(item['id'],item['num_members'],item['is_archived'],item['name']))
+
+#int(grupos_slack['channels'])
+#for grupo in gruposels_slack:
+#    print(grupo)
